f7954e79bd02a5fbff5784a67e03a1d503cb2ff5
gengliqi <gengliqiii@gmail.com>
2023-04-11 18:32:34 +0800

    use non_temporal_store_memcpy_avx
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/Interpreters/Join.cpp
+++ dbms/src/Interpreters/Join.cpp
@@ -896,7 +896,7 @@ void NO_INLINE insertFromBlockImplTypeCaseWithLock(
                 auto * p = batch_pointers[segment_index];
                 size_t size = p->size();
                 p->resize(size + buffer_size);
-                absl::crc_internal::non_temporal_store_memcpy(&(*batch_pointers[segment_index])[size], &buffer[segment_index * buffer_size], buffer_size * sizeof(DataType));
+                absl::crc_internal::non_temporal_store_memcpy_avx(&(*batch_pointers[segment_index])[size], &buffer[segment_index * buffer_size], buffer_size * sizeof(DataType));
 
                 if (unlikely(p->size() >= min_batch_insert_ht_size))
                 {
@@ -1113,7 +1113,7 @@ void insertRemainingImplType(
         {
             size_t size = b->size();
             b->resize(size + batch.write_pos[segment_index]);
-            absl::crc_internal::non_temporal_store_memcpy(&(*b)[size], &buffer[segment_index * buffer_size], batch.write_pos[segment_index] * sizeof(DataType));
+            absl::crc_internal::non_temporal_store_memcpy_avx(&(*b)[size], &buffer[segment_index * buffer_size], batch.write_pos[segment_index] * sizeof(DataType));
         }
 
         if (b->empty())
