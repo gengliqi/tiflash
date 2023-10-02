df26ccd3ba63527277f64ac95207ae9c262b5fa6
gengliqi <gengliqiii@gmail.com>
2023-10-02 17:54:30 +0800

    update
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/Server/ComputeEngineInterface.h
+++ dbms/src/Server/ComputeEngineInterface.h
@@ -6,9 +6,9 @@ extern "C" {
 
 #include <stdint.h>
 
-#define MY_API __attribute ((visibility ("default")))
+#define EXPORT __attribute ((visibility ("default")))
 
-MY_API void runComputeEngine(char * config);
+EXPORT void runComputeEngine(char * config);
 
 typedef struct RawString
 {
@@ -16,19 +16,19 @@ typedef struct RawString
     uint32_t size;
 } RawString;
 
-MY_API RawString newRawString(void * data, uint32_t size);
-MY_API void deleteRawString(RawString s);
+EXPORT RawString newRawString(void * data, uint32_t size);
+EXPORT void deleteRawString(RawString s);
 
 // TODO: use local context instead of global context
 extern void * global_flash_context;
 
-MY_API int dispatchMPPTask(void * ctx, RawString raw_request, RawString * raw_response);
+EXPORT int dispatchMPPTask(void * ctx, RawString raw_request, RawString * raw_response);
 
 typedef struct MPPStreamResponse MPPStreamResponse;
 
-MY_API int establishMPPConnection(void * ctx, RawString raw_request, MPPStreamResponse ** stream_response);
-MY_API int nextResponse(MPPStreamResponse * stream_response, RawString * raw_response);
-MY_API void deleteMPPStreamResponse(MPPStreamResponse * stream_response);
+EXPORT int establishMPPConnection(void * ctx, RawString raw_request, MPPStreamResponse ** stream_response);
+EXPORT int nextResponse(MPPStreamResponse * stream_response, RawString * raw_response);
+EXPORT void deleteMPPStreamResponse(MPPStreamResponse * stream_response);
 
 MY_API int cancelMPPTask(void * ctx, RawString raw_request, RawString * raw_response);
 
