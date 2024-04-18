17736640e1cb40cca2078cdca7cc413fc6800ffb
gengliqi <gengliqiii@gmail.com>
2024-04-18 17:36:24 +0800

    update again
    Signed-off-by: gengliqi <gengliqiii@gmail.com>

--- dbms/src/DataStreams/ScanHashMapAfterProbeBlockInputStream.cpp
+++ dbms/src/DataStreams/ScanHashMapAfterProbeBlockInputStream.cpp
@@ -666,7 +666,7 @@ void ScanHashMapAfterProbeBlockInputStream::fillColumnsNewImpl(
 
         scan_rows += current_index - prev_index;
         total_scan_rows += current_index - prev_index;
-        if unlikely (added_rows >= max_block_size || (added_rows > 0 && scan_rows >= max_block_size * 8))
+        if unlikely (added_rows >= max_block_size || (added_rows > 0 && scan_rows >= max_block_size * 2))
             break;
     }
     if (!row_ptrs_buffer.empty())
